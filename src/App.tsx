9ebb8862de64960a8511ffa0439549cf35a041d4
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-04-19 18:57:47 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,42 @@ import '@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [messages, setMessages] = useState([
+        {
+            message: "Hello, I am ChatGPT!",
+            sender: "ChatGPT"
+        }
+    ]
+    );
 
+    const handleSend = async (message: string) => {
+        const newMessage = {
+            message: message,
+            sender: "user",
+            direction: "outgoing"
+        }
+
+        const newMessages = [...messages, newMessage];
+
+        // Update our messages state
+        setMessages(newMessages);
+
+
+
+        // process message to chatGPT (send it over and see the response)
+
+    }
     return (
         <div className={styles.App}>
             <div style={{ position: "relative", height: "800ox", width: "700px" }}>
                 <MainContainer>
                     <ChatContainer>
                         <MessageList>
-                            
+                            {messages.map((message, i) => {
+                                return <Message key={i} model={message} />
+                            })}
                         </MessageList>
+                        <MessageInput placeholder="Type message here..." onSend={handleSend} />
                     </ChatContainer>
                 </MainContainer>
             </div>
